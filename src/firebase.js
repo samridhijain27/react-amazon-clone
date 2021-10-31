8f0d7423ea402ddbfeb3e6ca3f2e62348e8cf9d6
Saloni gupta <gupta.saloni.1297@gmail.com>
2021-10-31 23:10:55 +0530

    Removed sensitive information (api key)

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCq8PFyutmag5TJn1JCsiZXavTDg9oj6LI",
+    apiKey: XX,
     authDomain: "clone-3628b.firebaseapp.com",
     projectId: "clone-3628b",
     storageBucket: "clone-3628b.appspot.com",
-    messagingSenderId: "13133422011",
-    appId: "1:13133422011:web:d82343ddbf61a8d2af789e",
+    messagingSenderId: XX,
+    appId: XX,
     measurementId: "G-5FMDP07J7T,
 });
 const db = firebaseApp.fireStore();
